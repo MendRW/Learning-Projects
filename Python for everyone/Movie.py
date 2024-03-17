ec7fec44244138d047324b0f8d6d1fa4aff78e2d
MendRW <rwilc13@gmail.com>
2024-03-17 20:02:46 +1000

    Py help
    helping friend with py project

---
+++ Python for everyone/Movie.py
@@ -0,0 +1,16 @@
+
+Favouritemovie = False
+while Favouritemovie == False:
+    print("What movie do you want to watch?")
+    answer = input()
+    if answer =="WALL-E":
+        Favouritemovie = True
+        break
+    else:
+        print("That was fun. Let's watch another one.")
+        continue
+print("That was my favourite movie of all time.")
+print("I think I'll power down now.")
+    
+
+

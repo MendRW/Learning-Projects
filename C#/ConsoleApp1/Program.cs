b2c54fd82028d01ea4f4b2cd5e5435009be06d61
MendRW <rwilc13@gmail.com>
2025-02-21 20:09:08 +1000

    twosum

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+    public class Solution
+     {
+        public int[] TwoSum(int[], int target)
+        {
+            for (int i = 0; i <nums.Length; i++)
+            {
+                
+            }
+        }
+    }
--- C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a607fd1e102773ab2b12f01cfe08491671441ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02e3a37a794131766198e3dc71355e9827b7f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

e27fcece8256c205c797932a6087a8f3cc7b05d4
MendRW <rwilc13@gmail.com>
2025-03-22 22:50:50 +1000

    .

--- Data Annotation/Untitled-1.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-file_path = Path('C:/Users/rwilc/OneDrive/Documents/GitHub/GithubTest/Data Annotation/annodata.txt')
-print(file_path)
-
-with open(file_path, 'r', encoding='utf-8') as file:
-    content = file.read()
-print(content)
